078980f5e90996540a34322a04fabe08cf814a2b
Donali <00084417@uca.edu.sv>
2018-07-08 21:20:34 -0600

    Added assets and configured working directory

---
+++ desktop/src/com/dona/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dona.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dona.JackGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JackGame(), config);
+	}
+}
