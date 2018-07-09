9b7e05b5a528cbe844a20a715afc53aab4d6278f
Donali <00084417@uca.edu.sv>
2018-07-08 22:19:00 -0600

    Created Camera

--- core/src/scenes/GamePlay.java
+++ core/src/scenes/GamePlay.java
@@ -3,19 +3,39 @@ package scenes;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.dona.JackGame;
+import helpers.GameInfo;
 
 public class GamePlay implements Screen {
 
     private JackGame jackGame;
     private Sprite[] bgs;
 
+    //Camara que seguira al Sprite
+    private OrthographicCamera mainCamera;
+    //Punto de vista de la pantalla que se asignara a la mainCamera
+    private Viewport viewPort;
+
     public GamePlay(JackGame jackGame) {
         this.jackGame = jackGame;
+        mainCamera = new OrthographicCamera(GameInfo.WIDTH,GameInfo.HEIGHT);
+        mainCamera.position.set(GameInfo.WIDTH / 2f, GameInfo.HEIGHT/2f,0);
+        viewPort = new StretchViewport(GameInfo.WIDTH,GameInfo.HEIGHT,mainCamera );
         createBackGrounds();
 
+    }
+
+    private void update(float dt){
+        moveCamera();
+    }
+
+    private void moveCamera(){
+        mainCamera.position.y-=1;
     }
 
     //Creando y posicionando los fondos uno detras del otro
@@ -23,7 +43,7 @@ public class GamePlay implements Screen {
         bgs = new Sprite[3];
         for (int i = 0; i<bgs.length;i++){
             bgs[i] = new Sprite(new Texture("Backgrounds/Game BG.png"));
-            bgs[i].setPosition(0,(i* bgs[i].getHeight()));
+            bgs[i].setPosition(0,-(i* bgs[i].getHeight()));
         }
     }
 
@@ -40,6 +60,8 @@ public class GamePlay implements Screen {
 
     @Override
     public void render(float delta) {
+        //Actualizando a la velocidad delta que es lo que toma de un cuadro a otro
+        update(delta);
         //Limpiando la pantalla antes de dibujar algo en ella
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -52,6 +74,9 @@ public class GamePlay implements Screen {
 
         //Terminando de dibujar
         jackGame.getBatch().end();
+
+        jackGame.getBatch().setProjectionMatrix(mainCamera.combined);
+        mainCamera.update();
     }
 
     @Override
